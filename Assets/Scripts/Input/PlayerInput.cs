41f951a434964ab8e45bbc0980138d3b6405d5a7
Kieran Schwegman <schwegmank@gmail.com>
2021-04-21 21:17:47 -0400

    Initial code
    This is before the state machine was implemented. All the conditionals for physics andd movement got tangled and messy

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Character Inputs
+    [HideInInspector] public float movementInput;
+    [HideInInspector] public bool jump;
+    [HideInInspector] public bool crouch;
+    [HideInInspector] public bool holdingJump;
+    [HideInInspector] public bool holdingCrouch;
+    [HideInInspector] public bool dash;
+    [HideInInspector] public bool attack;
+    [HideInInspector] public bool autoAttack;
+
+    // Menu Inputs
+    //[HideInInspector] public bool pause; // esc
+    //[HideInInspector] public bool menu; // tab
+    //[HideInInspector] public bool inventory; // i
+
+    void Update()
+    {
+        // Grab the direction they're pressing
+        movementInput = Input.GetAxis("Horizontal");
+
+        // Jump inputs
+        jump = Input.GetButtonDown("Jump") ? true : false;
+        holdingJump = Input.GetButton("Jump") ? true : false;
+
+        // Crouch inputs
+        crouch = Input.GetButtonDown("Crouch") ? true : false;
+        holdingCrouch = Input.GetButton("Crouch") ? true : false;
+
+        // Dash inputs
+        dash = Input.GetButtonDown("Dash") ? true : false;
+
+        // attack inputs
+        attack = Input.GetButtonDown("Attack") ? true : false;
+        attack = Input.GetButton("Attack") ? true : false;
+    }
+}
---
+++ Assets/Scripts/Old/JumpGravity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class JumpGravity : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float downardJumpGravity = 10f;
+    [SerializeField] private float upwardJumpGravity = 10f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // Jump gravity logic
+        if (rb.velocity.y < 0) // Downwards gravity
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (downardJumpGravity - 1) * Time.fixedDeltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) // Upwards Gravity
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (upwardJumpGravity - 1) * Time.fixedDeltaTime;
+        }
+    }
+}
