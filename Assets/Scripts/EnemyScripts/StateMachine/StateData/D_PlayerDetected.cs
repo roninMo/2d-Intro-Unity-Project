9e067a401b67c8ede8d6540ad95eb2f653246785
Kieran Schwegman <schwegmank@gmail.com>
2021-05-23 14:55:28 -0400

    Tweaks to the wait time before the enemy charges

--- Assets/Scripts/EnemyScripts/StateMachine/StateData/D_PlayerDetected.cs
+++ Assets/Scripts/EnemyScripts/StateMachine/StateData/D_PlayerDetected.cs
@@ -3,5 +3,5 @@
 [CreateAssetMenu(fileName = "newPlayerDetectedStateData", menuName = "Data/State Data/PlayerDetected State")] 
 public class D_PlayerDetected : ScriptableObject
 {
-    public float longRangeActionTime = 1.5f;
+    public float longRangeActionTime = 0.5f;
 }
